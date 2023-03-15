a275ddf8ed9a7a506648748f0de6753b129d9261
josuedhc <50117293+josuedhc@users.noreply.github.com>
2023-03-14 21:03:17 -0600

    Add files via upload

---
+++ Proyecto1/index.php
@@ -0,0 +1,322 @@
+<!DOCTYPE html>
+<html lang="es">
+  <head>
+   <link rel="stylesheet" href="estilos/estilos.css">
+    <meta charset="utf-8">
+    <title>TOÑO DE MARCO </title>
+    <link href="https://fonts.googleapis.com/css?family=Poppins&display=swap" rel="stylesheet">
+	<link rel="stylesheet" href="owl/owl.carousel.min.css">
+    <link rel="stylesheet" href="owl/owl.theme.default.min.css">
+	<meta name="viewport" content="width=device-width, user-scalable=no">
+ </head>
+
+  <body>
+    <a id="whatsapp" href="https://api.whatsapp.com/send?phone=522225049929"><img src="imagenes/whatsapp.png"></a>
+    <header>
+      <div class ="menu">
+        <div class="contenedor">
+          <p class="logo">TOÑO DE MARCO</p>
+		  <img class="menu-icon" src="imagenes/menu.png">
+          <nav>
+            <ul class="lista-menu">
+              <li><a href="#sobre-mi">Sobre mi</a></li>
+              <li><a href="#servicios">Mis trabajos</a></li>
+              <li><a href="#mis-trabajos">¿Por que trabajar
+              conmigo?</a></li>
+              <li><a href="#blog">Sigueme</a></li>
+              <li><a href="#contacto"="">Contacto</a></li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+  <div class="contenedor" id="contenedor-titulo-flex">
+    <div class="contenedor-titulo">
+      <h1>TOÑO DE MARCO</h1>
+      <h2>COMEDIANTE IMITADOR </h2>
+      <a href="">Ver mis fotos</a>
+    </div>
+  </div>
+    </header>
+
+<main>
+<section id="sobre-mi">
+  <div class="contenedor">
+      <h3>Sobre mi </h3>
+      <div class="contenedor-sobremi">
+        <div class="computadoras">
+          <img src="imagenes/pastor.jpg">
+        </div>
+        <div class="texto">
+          <p>El pastor alemán u ovejero alemán es una raza canina
+             que proviene de Alemania.​ La raza es relativamente nueva,
+              ya que su origen se remonta a 1899.​
+               Forman parte del grupo de pastoreo,
+               debido a que fueron perros desarrollados originalmente para reunir y vigilar ovejas..
+          </p>
+          <p>Káiser (< alemán Kaiser < alto alemán antiguo keisar
+             < gótico *kaisar < latín Caesar ['césar, emperador'])​
+             es el título alemán que significa emperador,
+             siendo en alemán kaiserin su equivalente femenino, es decir, emperatriz.
+          </p>
+          <p>La noche es perfecta si tu estas a mi lado.
+          </p>
+          <a href="">Ver mis trabajos</a>
+        </div>
+      </div>
+  </div>
+</section>
+<section id="servicios">
+  <div class="contenedor">
+    <h3>Servicios</h3>
+     <div class="contenedor-servicios">
+		  <div class="servicio violeta">
+			<h4>Pastor Aleman Pelo Largo</h4>
+			  <p>Pastor aleman Pelo largo blablalblablalllbal</p>
+				<img class ="icono" src= "imagenes/icono.png">
+				<img class="ondas" src= "imagenes/ondas.jpg">
+		  </div>
+		   <div class="servicio celeste">
+			<h4>Pastor Aleman Pelo Largo</h4>
+			  <p>Pastor aleman Pelo largo blablalblablalllbal</p>
+				<img class = "icono" src= "imagenes/hueso.png">
+				<img class="ondas" src="imagenes/ondas.jpg">
+		  </div>
+		   <div class="servicio violeta">
+			<h4>Pastor Aleman Negro Solido</h4>
+			  <p>El origen de la raza del Perro Pastor Alemán negro
+			  se relaciona con dos campos: su descubrimiento y su gen.
+			  En el caso del primer campo, el descubrimiento de la raza,
+			  esto ocurrió en el año 1899. Se puede decir que es una raza de las
+			  más recientes.
+             </p>
+				<img class= "icono" src= "imagenes/PerroH.png">
+				<img class="ondas" src="imagenes/ondas.jpg">
+		  </div>
+		   <div class="servicio celeste">
+			<h4>Pastor Aleman Pelo Largo</h4>
+			  <p>Pastor aleman Pelo largo blablalblablalllbal</p>
+				<img class="icono" src="imagenes/icono.png">
+				<img class="ondas" src="imagenes/ondas.jpg">
+		  </div>
+		   <div class="servicio violeta">
+			<h4>Pastor Aleman Pelo Largo</h4>
+			  <p>Pastor aleman Pelo largo blablalblablalllbal</p>
+				<img class= "icono" src="imagenes/hueso.png">
+				<img class="ondas" src="imagenes/ondas.jpg">
+		  </div>
+		   <div class="servicio celeste">
+			<h4>Pastor Aleman Pelo Largo</h4>
+			  <p>Pastor aleman Pelo largo blablalblablalllbal</p>
+				<img class= "icono" src="imagenes/PerroH.png">
+				<img class="ondas" src="imagenes/ondas.jpg">
+		  </div>
+
+		  <div class="servicio">
+		  <!-- <h4>Responsive Design</h4> -->
+		  </div>
+     </div>
+  </div>
+</section>
+
+  <section id= "mis-trabajos">
+	<div class="contenedor">
+	 <h3>IMITADORES </h3>
+		<div class="owl-carousel owl-theme">
+			<div class="item"><a href="www.acafintex.com"><img src="imagenes/perro1.png"></a></div>
+			<div class="item"><a href="www.acafintex.com"><img src="imagenes/perro2.png"></a></div>
+      	    <div class="item"><a href="www.acafintex.com"><img src="imagenes/perro3.png"></a></div>
+        	<div class="item"><a href="www.acafintex.com"><img src="imagenes/perro4.png"></a></div>
+          	<div class="item"><a href="www.acafintex.com"><img src="imagenes/perro5.png"></a></div>
+            <div class="item"><a href="www.acafintex.com"><img src="imagenes/perro6.png"></a></div>
+            <div class="item"><a href="www.acafintex.com"><img src="imagenes/perro7.png"></a></div>
+		</div>
+    </div>
+  </section>
+  <section id="por-que-trabajar">
+	  <div class="contenedor">
+		<h3>¿Por que trabajar conmigo?</h3>
+			<div class="contenedor-trabajar">
+				 <div class="item">
+				  <ul>
+				   <li><img src="imagenes/Velo.png" alt=""></li>
+				     <li>DIVERSIÓN</li>
+				      <li>Me aseguro que tu web cargue entre dos
+					  o tres segundos (depende de la cantidad
+					  de contenido del sitio).</li>
+				  </ul>
+				 </div>
+				 <div class="item">
+				  <ul>
+				   <li><img src="imagenes/herramientas.png" alt=""></li>
+				     <li>CALIDAD</li>
+				      <li>Mantengo una comunicación constante
+					  con mi cliente, para enseñarle a
+					  modificar contenido mediante el panel
+					  de wordpress o para cualquier
+					  duda/cambio que quisiera hacer en su
+					  sitio web.</li>
+				  </ul>
+				 </div>
+				 <div class="item">
+				  <ul>
+				   <li><img src="imagenes/movil.png" alt=""></li>
+				     <li>HUMOR</li>
+				      <li>Cada vez más personas navegan desde
+					  sus celulares en cualquier parte del
+					  mundo. Me aseguro de que tu web esté
+					  optimizada para cualquier tipo de
+					  dispositivo móvil.</li>
+				  </ul>
+				 </div>
+			</div>
+	  </div>
+  </section>
+  <section id="blog">
+	<div class="contenedor">
+		<h3>SIGUEME</h3>
+		  <div class="contenedor-publicaciones">
+			 <div class="publicacion">
+				<img src="imagenes/marketing.jpg" alt="">
+					<div class="contenido">
+					 <h4>No alcanza con tener solamente una
+					 página web</h4>
+						<p>¿Quieres aprender cuales son las
+						herramientas que usan las mejores marcas
+						para generar tráfico? </p>
+						<a href="">Leer más → </a>
+					</div>
+			 </div>
+			  <div class="publicacion">
+				<img src="imagenes/marketing.jpg" alt="">
+					<div class="contenido">
+					 <h4>¿Que es el remarketing y
+						como utlilizarlo?</h4>
+						<p>El remarketing es una de las
+							ténicas más utilizadas por las mejores
+							compañias! ¿Quieres saber de que se
+							trata?</p>
+						<a href="">Leer más →</a>
+					</div>
+			 </div>
+
+		  </div>
+	</div>
+  </section>
+
+  <section id="que-esperas">
+	<div class="contenedor">
+		<h3>¿Qué esperas para tener tu pagina
+		web?</h3>
+		<a href="">CHARLEMOS</a>
+	</div>
+  </section>
+
+<section id="contacto">
+  <?php
+      if(@$_GET['i']=='ok') { // QUIERE DECIR QUE EL FORMULARIO SE ENVIO CORRECTAMENTE ?>
+
+      <h3>La consulta se envio correctamente. Nos contactaremos a la brevedad.</h3>
+
+      <?php
+      } else{
+      ?>
+  <div class="contenedor">
+    <h3>Contacto</h3>
+    <form action="envio-formulario.php" method="post" enctype="multipart/form-data">
+      <input type="text" placeholder="Nombre" name="nombre" required>
+      <input type="email" placeholder="Email" name="email" required>
+      <textarea placeholder="Mensaje" name="mensaje" required></textarea>
+      <input type="submit" value="ENVIAR MENSAJE">
+    </form>
+          <?php } ?>
+
+	<div class="contacto-info">
+		<div class="mail"> <img src="imagenes/mail-icon.png" alt="">
+		info@toñodemarco.com
+		</div>
+		<div class="whatsapp"> <img src="imagenes/whatsapp-icon.png" alt="">2225049929
+		</div>
+	</div>
+
+
+
+  </div>
+</section>
+
+  <footer>
+	<div class="contenedor">
+		<div class="redes">
+			<a href="#"><img src="imagenes/facebook.png" alt="imagen de facebook"></a>
+			<a href="#"><img src="imagenes/medium.png" alt="imagen de facebook"></a>
+			<a href="#"><img src="imagenes/linkedin.png" alt="imagen de facebook"></a>
+			<a href="#"><img src="imagenes/github.png" alt="imagen de facebook"></a>
+
+
+		</div>
+		<div class="parrafo">
+			<p>2019 Josué David Hernández Cuellar | Desarrollador WEB. Todos los derechos
+			reservados.</p>
+		</div>
+	</div>
+  </footer>
+
+
+</main>
+	<script
+    src="https://code.jquery.com/jquery-3.4.1.min.js"
+    integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+    crossorigin="anonymous"></script>
+   <script src="jquery.min.js"></script>
+
+   <script src="owl/owl.carousel.min.js"></script>
+   <script>
+   $('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+	dots: false,
+	autoplay:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:4
+        }
+    }
+	})
+   </script>
+
+   <script>
+  $(document).ready(function(){
+		
+      $(window).scroll(function(){
+        scroll =$(window).scrollTop();
+        if (scroll > 100){
+          $('.menu').css({"position":"fixed"});
+          $('.menu').css({"width":"100%"});
+          $('.menu').css({"top":"0"});
+          $('.menu').css({"background":"#fff"});
+          $('.menu a').css({"color":"#000"});
+          $('.logo').css({"color":"#000"});
+          $('.menu').css({"box-shadow":"rgba(0, 0, 0, 0.22) 6px 1px 1px"});
+          $('.menu').css({"z-index":"100"});
+        } else {
+			//cuando no hay scroll
+          $('.menu').css({"position":"relative"});
+          $('.menu').css({"background":"transparent"});
+          $('.menu').css({"box-shadow":"0 0 0"});
+          $('.menu a').css({"color":"#fff"});
+          $('.logo').css({"color":"#fff"});
+        }
+      })
+	  $('.menu-icon').click(function(){
+      $('header nav').slideToggle(); 
+      })
+})
+   </script>
+  </body>
+</html>
